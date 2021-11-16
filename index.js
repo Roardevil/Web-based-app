39a481c850395aa1ff060ba4a95424c9ab835ab6
Roardevil <pateldeep25022001@gmail.com>
2021-11-16 13:33:44 +0000

    Added search bar and sort

--- index.js
+++ index.js
@@ -1,29 +1,152 @@
-
-let app = new Vue({
-    el: "#app",
+var app = new Vue({
+    el: "#vueapp",
     data: {
         lesson: lesson,
         showproduct: true,
+        filter: '',
+        sort: '',
+        terms: false,
+        Country: {
+            UK: 'united kingdom',
+            IN: 'India',
+            US: 'United states of america',
+            CA: 'Canada',
+            AUS: 'Australia'
+        },
+        order: {
+            firstName: '',
+            lastName: '',
+            address: '',
+            city: '',
+            postcode: '',
+            Country: '',
+            method: 'Home Address',
+            business: 'Business Address',
+            home: 'Home Address',
+            gift: 'Send As A Gift',
+            sendGift: 'Send As A Gift',
+            dontSendGift: 'Do Not Send As A Gift'
+        },
+        cart: [],
 
+        options: [
+            { label: 'Default', value: 'none' },
+            { label: 'Most Rated', value: 'rating' },
+            { label: 'least Rated', value: 'leastrated' },
+            { label: 'price(high to low)', value: 'price(high to low)' },
+            { label: 'price(low to high)', value: 'price(low to high)' },
+
+
+        ],
+        errors: [],
+        name: null,
+        age: null,
+        movie: null
 
     },
-
-
-
-
     methods: {
-        //  This function decrease the value of stock by 1 every time user click Button
+        additem(id) {
+            if (lesson[id].space > 0) {
+                this.space = --lesson[id].space;
 
-        additem: function () {
-            if (lesson.stock >= 0) {
-                this.space = --lesson.stock;
+
+                this.cart.push(id);
             }
         },
 
+        showcheckout() {
+            this.showproduct = this.showproduct ? false : true;
+        },
+        canaddtocart(aProduct) {
+
+            return aProduct.space > this.cartCount(aProduct.id);
+        },
+        cartCount(id) {
+            let count = 0;
+            for (var i = 0; i < this.cart.length; i++) {
+                if (this.cart[i] === id) {
+                    count++;
+                }
+            }
+            return count;
+        },
+
+
+        submitForm() {
+            alert('Submitted');
+        },
+        checkForm: function (e) {
+            if (this.firstName && this.lastName && this.address && this.city && this.postcode) return true;
+            this.errors = [];
+            if (!this.firstName) this.errors.push("First Name required.");
+            if (!this.lastName) this.errors.push("Last Name required.");
+            if (!this.address) this.errors.push("Address required.");
+            if (!this.city) this.errors.push("City required.");
+            if (!this.postcode) this.errors.push("Postcode required.");
+
+            e.preventDefault();
+        },
+        removeButton(index) {
+            // this.lesson[index].space++;
+            for (let i = 0; i < this.cart.length; i++) {
+                if (this.cart[i] == index) {
+                    this.cart.splice(i, 1);
+                    console.log(i + " " + index);
+                    break;
+
+                }
+            }
+        }
+
+
+
+
+
+
     },
-    computed: {// This disables the button at 0 stock
+    computed: {
         isDisabled() {
-            return this.stock === 0;
+
+        },
+        cartItemCount() {
+            return this.cart.length || '';
+        },
+
+        getlesson() {
+
+            var lesson = this.lesson.filter((lesson) => {
+                return lesson.subject.toLowerCase().includes(this.filter.toLowerCase());
+            });
+
+            if (this.sort == 'rating') {
+                return lesson.sort(function (a, b) {
+                    return b.rating - a.rating
+                });
+
+            }
+            else if (this.sort == 'leastrated') {
+                return lesson.sort(function (a, b) {
+                    return a.rating - b.rating
+                });
+
+            }
+            else if (this.sort == 'price(high to low)') {
+                return lesson.sort(function (a, b) {
+                    return b.price - a.price
+                });
+
+            }
+            else if (this.sort == 'price(low to high)') {
+                return lesson.sort(function (a, b) {
+                    return a.price - b.price
+                });
+
+            }
+
+            else {
+                return lesson;
+            }
+
         }
     }
-});
+})
---
+++ lesson.js
@@ -0,0 +1,120 @@
+let lesson = [
+    {
+        id: 0,
+        subject: "Maths",
+        location: "london",
+        price: 180,
+        space: 8,
+        image: "../images/maths.jpg",
+        rating: 3,
+
+
+    },
+
+    {
+        id: 1,
+        subject: "English",
+        location: "london",
+        price: 80,
+        space: 7,
+        image: "../images/english.jpg",
+        rating: 4,
+
+
+
+    },
+    {
+        id: 2,
+        subject: "Music",
+        location: "london",
+        price: 100,
+        space: 8,
+        image: "../images/music.png",
+        rating: 2,
+
+
+    },
+    {
+        id: 3,
+        subject: "Dance",
+        location: "london",
+        price: 150,
+        space: 10,
+        image: "../images/dance.jpg",
+        rating: 4,
+
+
+    },
+    {
+        id: 4,
+        subject: "Swimming",
+        location: "london",
+        price: 160,
+        space: 12,
+        image: "../images/swim.png",
+        rating: 3,
+
+
+
+    },
+    {
+        id: 5,
+        subject: "Science",
+        location: "london",
+        price: 300,
+        space: 12,
+        image: "../images/science.jpg",
+        rating: 5,
+
+
+    },
+    {
+        id: 6,
+        subject: "Drawing",
+        location: "london",
+        price: 20,
+        space: 12,
+        image: "../images/draw.jpg",
+        rating: 1,
+
+
+    },
+    {
+        id: 7,
+        subject: "Acting",
+        location: "london",
+        price: 100,
+        space: 8,
+        image: "../images/act.jpg",
+        rating: 2,
+
+
+    },
+    {
+        id: 8,
+        subject: "Hindi",
+        location: "london",
+        price: 400,
+        space: 6,
+        image: "../images/hindi.jpg",
+        rating: 5,
+
+    },
+    {
+        id: 9,
+        subject: "Singing",
+        location: "london",
+        price: 260,
+        space: 7,
+        image: "../images/sing.jpg",
+        rating: 4,
+
+
+    },
+
+
+
+
+
+
+]
